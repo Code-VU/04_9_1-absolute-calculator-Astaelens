56e2afecd471f9a02fe40608bc6dbabe09813067
Alex Staelens <Alex.Staelens@vu.com>
2023-02-11 15:37:37 -0600

    added if statement to calculate result

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -1,7 +1,13 @@
 def calculateAbsolute():
     
     # This first line is provided for you
-    in_num  = input("Enter a number: ")
+    in_num  = int(input("Enter a number: "))
+
+    if in_num>=21:
+        abs_num=2*(in_num-21)
+    else:
+        abs_num=21-in_num
+    print("Result:", abs_num)
     # end assignment
 
 ## If you want to test locally run > python payCalculator.py
